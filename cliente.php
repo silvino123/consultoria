c584dedeec3c9c2d817ef243676e54297e9b40da
silvino123 <36116136+silvino123@users.noreply.github.com>
2019-08-24 11:11:01 -0700

    vv

--- cliente.php
+++ cliente.php
@@ -93,49 +93,7 @@
                             <br>
                             <div class="row">
                             <div class="col-lg-6">
-                            <div class="table-responsive">
-                            <table class="table table-bordered table-striped">
-                              <thead>
-                              <tr>
-                              <th colspan="2" class="text-center" style="background-color:#1c84c6;color:#fff" >Reporte general de pruebas aplicadas</th>
-                              </tr>
-                              </thead>
-                              <tbody>
-                              <tr>
-                              <td class="text-center" style="background-color:#d2d2d2;color:#000000"><b>Area<b></td>
-                              <td class="text-center" style="background-color:#d2d2d2;color:#000000"><b>Total<b></td>
-                              
-                              </tr>
-                              <tr>
-                              <td class="text-center"><b>Area 1<b></td>
-                              <td class="text-center" style="color:#000000"><b>53<b></td>
-                              </tr>
-                              <tr>
-                              <td class="text-center"><b>Area 2<b></td>
-                              <td class="text-center" style="color:#000000"><b>30<b></td>
-                              </tr>
-                              <tr>
-                              <td class="text-center"><b>Area 3<b></td>
-                              <td class="text-center" style="color:#000000"><b>15<b></td>
-                              </tr>
-                              <tr>
-                              <td class="text-center"><b>Area 4<b></td>
-                              <td class="text-center" style="color:#000000"><b>20<b></td>
-                              </tr>
-                              <tr>
-                              <td class="text-center"  style="background-color:#1c84c6;color:#fff"><b>Total<b></td>
-                              <td class="text-center" style="background-color:#18a689;color:#fff"><b>160<b></td>
-                              </tr>
-                              <tr>
-                              
-                              </tr>
-                              <tr>
-                              
-                              </tr>
-                              
-                              </tbody>
-                            </table>
-                            <div>
+                            <?php include 'pruebasAreas.php'; ?>
                             </div>
                             </div>
                             <!-- final row div -->
